cefbe28e586a8d71ec532631892e76283494648e
DawidBochon <65425838+DawidBochon@users.noreply.github.com>
2020-05-15 23:19:35 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,365 @@
+questions = 
+    [[//Federal-Unitary`
+        [//4
+            "Ogólnokrajowy rząd jest niepotrzebny.",
+            "Niższe jednostki w podziale terytorialnym państw powinny mieć swoje własne armie.",
+            "Prawa stanowe powinny mieć pierwszeństwo nad prawami narodowymi."
+        ],
+        [//3
+            "Prawa stanowe oraz lokalne powinny mieć pierwszeństwo nad prawami narodowymi.",
+            "Ogólnokrajowy rząd nie jest w stanie zrozumieć, czego ludzie potrzrebują.",
+            "Ludzie powinni być bardziej lojalni swoim lokalnym stanom, niż swojemu państwu."
+        ],
+        [//2
+            "Władze lokalne powinny posiadać więcej uprawnień, niż obecnie.",
+            "Prawa powinny się różnić w zależności od regionu oraz kultury w granicach kraju.",
+            "Lepszym pomysłem jest przetestować działanie zasady w granicach jednego stanu niż od razu wprowadzać ją na terenie całego kraju."
+            
+        ],
+        [//1
+            "Władze lokalne mogą zrozumieć swoich obywateli lepiej niż mógłby rząd ogólnokrajowy",
+            "Władze lokalne zajmują się problemami, którymi rząd ogólnokrajowy bez ich pomocy nigdy by się nie zainteresował.",
+            "Władze lokalne dają każdemu regionowi dobrą reprezentację swojego punktu widzenia."
+        ],
+        [//-1
+            "Kraj nie może funkcjonować bez ogólnokrajowego rządu",
+            "Ogólnokrajowy rząd chroni mniejszości lepiej niż niektóre władze lokalne.",
+            "Jest wiele problemów, których nie ma sensu zawierzać władzom lokalnym."
+        ],
+        [//-2
+            "Ogólnokrajowy rząd musi mieć większą władzę.",
+            "Niższe jednostki w podziale terytorialnym nie powinny być w stanie zwalniać z praw ogólnokrajowych.",
+            "Ludzie powinni uważać się za obywateli swojego kraju, a nie stanu."
+        ],
+        [//-3
+            "Grupa stanów bez silnego ogólnokrajowego rządu jest zaledwie sojuszem, nie krajem.",
+            "Rząd ogólnokrajowy musi być silny, żeby adekwatnie chronić wszystkich swoich obywateli.",
+            "Stany nie mogą funkcjonować bez silnego ogólnokrajowego rządu."
+        ],
+        [//-4
+            "Władze lokalne lub stanowe są niepotrzebne.",
+            "Prawa powinny być kompletnie jednolite we wszystkich regionach kraju.",
+            "Silne władze stanowe osłabiają kraj."
+        ]],[//Democratic-Authoritarian`
+        [//4
+            "Demokracja bezpośrednia jest najlepszą formą rządów.",
+            "Ludzie sami powinni głosować nad każdym problemem.",
+            "Reprezentanci rzadko dobrze reprezentują naród."
+        ],
+        [//3
+            "To ważne, by rząd postępował zgodnie z opinią większości, nawet jeśli jest błędna.",
+            "Im więcej ludzi jest pytanych, tym większa szansa na podjęcie najlepszej decyzji.",
+            "Nikt poza mną nie może adekwatnie reprezentować moich poglądów."
+        ],
+        [//2
+            "Wybory są skutecznym sposobem na pokazanie poparcia dla rządu.",
+            "Demokracja jest najgorszą formą rządu, jeśli nie liczyć wszystkich innych form, których próbowano od czasu do czasu.",
+            "Każda osoba powinna posiadać jeden głos, który jest równy wobec głosów innych."
+        ],
+        [//1
+            "Demokracja to więcej niż proces podejmowania decyzji.",
+            "Ludzie powinni mieć prawo do wybierania swoich przywódców.",
+            "Rząd musi być od ludzi dla ludzi."
+        ],
+        [//-1
+            "Im silniejsze przywództwo, tym lepiej.",
+            "Opinia większości nie zawsze jest właściwa.",
+            "Przywódcy muszą być silni, albo naród będzie cierpiał."
+        ],
+        [//-2
+             "Podział hierarchiczny jest najlepszy.",
+            "Społeczeństwo nie może funkcjonować bez rządzących i rządzonych.",
+             "Demokracja zbyt ułatwia większości sprzyjać sobie na koszt mniejszości."
+        ],
+        [//-3
+            "Większość populacji podejmuje złe decyzje.",
+             "To ważne, żeby sprzyjać celom mojej grupy ponad wszystkimi innymi.",
+            "Opinia publiczna zmienia zdanie zbyt często pod wpływem emocji, żeby być przydatna dla kształtowania polityki."
+        ],
+        [//-4
+            "Wybory to marnowanie zasobów.",
+            "Usługiwanie większości jest szkodliwe dla kraju.",
+            "Naród musi bezwarunkowo słuchać przywództwa."
+        ]],[//Globalist-Isolationist
+        [//4
+            "Zjednoczony rząd światowy byłby korzystny dla ludzkości.",
+            "Jesteśmy pierwszymi i czołowymi obywatelami Ziemii.",
+            "W celu przetrwania ludzkości, musimy przebrnąć przez posiadanie oddzielnych nacji."
+        ],
+        [//3
+            "Organizacja Narodów Zjednoczonych powinna mieć więcej władzy.",
+            "Organizacja Narodów Zjednoczonych powinna mieć armię, żeby egzekwować swoje postanowienia.",
+            "Posiadanie niepodległych krajów z ryzykiem globalnego konfliktu tworzy wielkie zagrożenie dla całej ludzkości."
+        ],
+        [//2
+            "Popieram organizacje międzynarodowe, takie jak Unia Europejska.",
+            "Rządy powinny być odpowiedzialne przed społecznością międzynarodową.",
+            "Państwa walczące przeciwko sobie stają na drodze postępu."
+        ],
+        [//1
+            "Handel międzynarodowy jest generalnie korzystny.",
+            "Sojusze są ważne, żeby chronić państwo.",
+            "Państwa powinny współpracować zawsze, kiedy tylko na tym zyskują."
+        ],
+        [//-1
+            "To ważne, żeby dbać o naszą narodową suwerenność.",
+            "Moje państwo oddaje moje poglądy lepiej niż większość państw na świecie.",
+            "Jestem dumny z bycia w moim kraju."
+        ],
+        [//-2
+            "Pomoc międzynarodowa to strata pieniędzy.",
+            "Sojusze mogą stanowić zagrożenie dla suwerenności państwa.",
+            "To ważne, żeby chronić narodowe kultury."
+        ],
+        [//-3
+            "Moje państwo powinno trzymać się z dala od afer międzynarodowych.",
+            "Organizacja Narodów Zjednoczonych powinna być zniesiona.",
+            "Nikt w innych krajach nie myśli w naszym najlepszym interesie."
+        ],
+        [//-4
+            "Dyplomacja tylko szkodzi państwom.",
+            "Nie powinno być praw międzynarodowych.",
+            "Wszystkie interesy, które chcą zawrzeć inne kraje muszą być dla nas złe."
+        ]],[//Militarist-Pacifist
+        [//4
+            "Wojna jest konieczna, żeby ekonomia dobrze się rozwijała.",
+            "Ciągły stan wojny przysłuża się krajowi.",
+            "Tylko ci, którzy służyli w wojsku powinni być uprawnieni do posiadania wpływu na to, kto jest u władzy."
+        ],
+        [//3
+            "Powinna być obowiązkowa służba wojskowa.",
+            "Kraje, które nie służom naszym interesom powinny upaść.",
+            "Tylko ci, którzy służyli w wojsku powinni móc zyskiwać władzę w rządzie."
+        ],
+        [//2
+            "Działanie militarne naszego państwa jest często konieczne, by je chronić.",
+            "Kraje, które łamią prawa człowieka powinny upaść.",
+            "Opcja wojskowa zawsze powinna być brana pod uwagę."
+        ],
+        [//1
+            "Rząd musi utrzymywać silną armię, żeby się obronić.",
+            "Państwo zazwyczaj potrzebuje wojska, żeby przetrwać.",
+            "Powinniśmy szanować tych, którzy służyli w wojsku."
+        ],
+        [//-1
+            "Ważniejsze jest utrzymać pokojowe relacje niż rozwijać naszą siłę.",
+            "Opcje dyplomatyczne powinny zawsze być próbowane, zanim wojna jest w ogóle rozważana.",
+            "Pokój jest bardziej pożądany od wojny, kiedy tylko możliwe"
+        ],
+        [//-2
+            "Nawet jeśli protestujemy przeciwko rządowi autorytarnemu, przemoc nie jest akceptowalna.",
+            "Trwały pokój prowadzi do dobrobytu.",
+            "Wojna zazwyczaj prowadzi do gorszych rezultatów."
+        ],
+        [//-3
+            "Wydatki na wojsko są stratą pieniędzy.",
+            "Nie mamy prawa ingerować militarnie w innych państwach.",
+            "Przy ciągłej wojnie, państwa upadają."
+        ],
+        [//-4
+            "Wojna nigdy nie jest uzasadniona.",
+            "W obecnych czasach, wojsko nie jest naprawdę konieczne.",
+            "Państwa nie powinny utrzymywać armii."
+        ]],[//Security-Freedom
+        [//4
+            "Ci, którzy krytykują rząd powinni zostać usunięci z kraju.",
+            "Rząd powinien śledzić wszystkich obywateli w celu walki z terroryzmem.",
+            "Ludzie nie powinni mieć zabezpieczeń, które mogłyby utrudnić odkrywanie ich działalności przestępczej."
+        ],
+        [//3
+            "Policja powinna mieć więcej władzy oraz ochrony.",
+            "To ważne, żeby utrzymywać prawo i porządek.",
+            "Kraj musi być pewnym podjęcia wszelkich środków, by wdrożyć swoje przepisy."
+        ],
+        [//2
+            "Jeżeli nie masz nic do ukrycia, nie powinno cię obchodzić, że władza ma dostęp do twoich rozmów.",
+            "Poświęcenie niektórych wolności obywatelskich jest niezbędne do uchronienia nas przed aktami terroryzmu.",
+            "Rządowa inwigilacja jest niezbędna we współczesnym świecie."
+        ],
+        [//1
+            "Władza powinna mieć dostęp do emaili podejrzewanych terrorystów.",
+            "Niektóre wolności muszą zostać poddane w celu utrzymania ludzi w bezpieczeństwie.",
+            "Absolutna wolność czyni społeczeństwo niebezpicznym."
+        ],
+        [//-1
+            "Marihuana powinna zostać zalegalizowana.",
+            "Ludzie powinni mieć zapewnioną wolność kiedy zawsze, kiedy tylko powoduje to niewielkie ryzyko niebezpieczeństwa.",
+            "Rząd nie ma żadnego interesu w zaglądaniu do życia prywatnego większości ludzi."
+        ],
+        [//-2
+            "Aborcja powinna być legalna we wszystkich przypadkach.",
+            "Policja powinna być bardziej regulowana.",
+            "Żaden autorytet nie powinien pozostać bezsporny."
+        ],
+        [//-3
+            "Przestępstwa bez ofiar (takie jak zażywanie narkotyków) nie powinny być przestępstwami w ogóle.",
+            "Większość narkotyków powinna zostać zalegalizowana.",
+            "Informatorzy powinni mieć silną ochronę."
+        ],
+        [//-4
+            "Samo istnienie panstwa jest zagrożeniem dla naszej wolności.",
+            "Policja nie powinna istnieć.",
+            "Wolność ludzi powinna nie mieć granic."
+        ]],[//Equality-Markets`
+        [//4
+            "Komunizm, jeśli wprowadzony poprawnie, byłby dobrą formą gospodarki.",
+            "Od każdego według jego zdolności, każdemu według jego potrzeb.",
+            "Równość wyników jest ważna."
+        ],
+        [//3
+            "Opresja korporacji jest bardziej niepokojąca niż opresja rządów.",
+            "Podatki powinny być podniesione dla bogatych, żeby oddać biednym.",
+            "Obiekty użyteczności publicznej, takie jak drogi i elektryczność powinny być państwowe."
+        ],
+        [//2
+            "Popieram finansowaną z podatków, powszechną służbę zdrowia.",
+            "Bogaci mają o wiele więcej niż potrzebują.",
+            "Podstawowe rzeczy konieczne do życia, takie jak opieka zdrowotna, są prawami."
+        ],
+        [//1
+            "To nizebędne, żeby rząd interweniował w ekonomię, żeby chronić konsumentów.",
+            "Ludzie powinni mieć równe szanse na sukces.",
+            "Ludzie urodzeni w bogatych rodzinach odnoszą niesprawiedliwy sukces, nawet kiedy nie są wykwalifikowani i nie pracują ciężko."
+        ],
+        [//-1
+            "Ludzie powinni mieć prawo zostawiać swój majątek potomstwu.",
+            "Nadmierna rządowa interwencja jest zagrożeniem dla gospodarki.",
+            "Gospodarki bez żadnego kapitalizmu upadną."
+        ],
+        [//-2
+            "Lepiej jest utrzymywać zrównoważony budżet niż zapewniać dobrobyt wszystkim obywatelom.",
+            "Kapitalizm jest lepszy niż jakakolwiek istniejąca alternatywa.",
+            "Branża zarządzana państwowo jest zazwyczaj złym pomysłem."
+        ],
+        [//-3
+            "Im wolniejszy rynek, tym wolniejsi ludzie.",
+            "Ludzie powinni musieć pracować za cokolwiek, co dostaną.",
+            "Biedni wybierają, żeby być biednymi."
+        ],
+        [//-4
+            "Rząd nie powinien regulować gospodarki w ogóle.",
+            "Rząd nie powinien rozbijać monopoli.",
+            "Ci, którzy nie pracują, powinni umrzeć."
+        ]],[//Secular-Religious
+        [//4
+            "Religia powinna zostać zakazana.",
+            "Wiara jest kompletnym bezsensem.",
+            "Religia musi zanikać, żeby społeczeństwo się rozwijało."
+        ],
+        [//3
+            "Ubiór religijny powinien zostać zakazany w miejscach publicznych.",
+            "Nie ma siły wyższej.",
+            "Rząd nie powinien finansować żadnych organizacji religijnych w żaden sposób."
+        ],
+        [//2
+            "Popieram małżeństwa homoseksualne.",
+            "Każda religia musi być postrzegana równo przez rząd.",
+            "Religia powinna zostać usunięta ze wszystkiego w naszym rządzie."
+        ],
+        [//1
+            "Religia nie przynależy do rządu.",
+            "Prawa nie powinny być bazowane na religii.",
+            "Musi być rozdział pomiędzy kościołem i państwem."
+        ],
+        [//-1
+            "Wierzę, że jest coś po śmierci.",
+            "Religia jest korzystna dla społeczeństwa.",
+            "Ludzie muszą móc podążać za religią."
+        ],
+        [//-2
+            "Wierzę w siłę wyższą.",
+            "Organizacje religijne często zapewniają lepszą pomoc dla biednych niż dostarcza rząd.",
+            "Dzieci powinny być edukowane w wartościach religijych."
+        ],
+        [//-3
+            "Religia zazwyczaj dostarcza prawdę.",
+            "Moje wartości religijne powinny być rozprzestrzenione tak bardzo, jak możliwe.",
+            "Seks pozamałżeński jest niemoralny."
+        ],
+        [//-4
+            "Teokracja jest dobrą metodą rządzenia.",
+            "Władza powinna wspierać i rozpoznawać tylko jedną religię.",
+            "Większość złych rzeczy zdarzających się na świecie jest spowodowana przez nasze oddalanie się od religii."
+        ]],[//Progressive-Traditional
+        [//4
+            "Jeżeli sztuczna inteligencja mogłaby rządzić bardziej efektywnie od ludzi, powinna rządzić.",
+            "Modyfikacja genetyczna jest pozytywna, nawet na ludziach.",
+            "Tradycja po prostu przeszkadza w postępie technologicznym."
+        ],
+        [//3
+            "Automatyzacja całościowo jest niezwykle dobra dla społeczeństwa.",
+            "Tradycje nie mają wartości same w sobie.",
+            "Skoro ludzie już ucierpieli dla rozwinięcia technologii, powinniśmy to wykorzystać."
+        ],
+        [//2
+            "Powinniśmy włożyć wysiłek w dostarczenie człowieka na Marsa w najbliższej przyszłości.",
+            "Testowanie produktów na zwierzętach jest etyczne.",
+            "Postęp technologiczny jest zazwyczaj dobry."
+            
+        ],
+        [//1
+            "Zmiany klimatu spowodowane przez ludzi są obecnie jednym z największych zagrożeń dla naszego trybu życia.",
+            "Regulacje środowiskowe są niezbędne.",
+            "Lepszy świat przyjdzie z automatyzacji, nauki i technologii."
+        ],
+        [//-1
+            "Gonić postęp za każdą cenę jest niebezpieczne.",
+            "Technologia nie zawsze jest korzystna.",
+            "Tradycje są ważne."
+        ],
+        [//-2
+            "Technologia negatywnie wpływa na współczesne społeczeństwo.",
+            "Genetyczne modyfikacje powinny być używane rzadko, jeśli w ogóle.",
+            "Utrzymywanie wartości rodzinnych jest podstawą."
+        ],
+        [//-3
+            "Społeczeństwo wiele lat temu było lepsze, niż teraz.",
+            "To ważne, żeby podrzymywać tradycje z naszej przeszłości.",
+            "Starsi w społeczeństwie znają dla niego najlepszą drogę."
+        ],
+        [//-4
+            "Medycyna tradycyjna jest często bardziej efektywna niż medycyna współczesna.",
+            "Internet powinien zostać zakazany.",
+            "Musimy wrócić na podstawową drogę życia bez współczesnych udogodnień."
+        ]],[//Assimilationist-Multiculturalist
+        [//4
+            "Cudzoziemcy nie powinni nigdy wjeżdżać do kraju.",
+            "Nie powinniśmy pozwalać na żadnych uchodźców.",
+            "Tylko nasze języki narodowe powinny być mówione w naszym kraju."
+        ],
+        [//3
+            "Tylko wyjątkowo utalentowani imigranci, jeśli już, powinni mieć pozwolenie na wjazd do kraju.",
+            "Jeśli już przyjmujemy migrantów, to ważne rzeby zasymilowali się w naszej kulturze.",
+            "Imigranci nigdy naprawdę nie zadaptują się do naszego stylu życia."
+        ],
+        [//2
+            "Ogólnie, imigranci są źli dla kraju.",
+            "To ważne, żeby chronić kulturę narodową.",
+            "Nielegalni imigranci nie zapewniają korzyści dla kraju goszczącego."
+        ],
+        [//1
+            "Nielegalni imigranci powinni być deportowani we wszystkich, albo w większości przypadków.",
+            "Powinno się spodziewać od imigrantów nauki języka państwa.",
+            "Ochrona granic jest ważna."
+        ],
+        [//-1
+            "Powinniśmy przyjmować więcej uchodźców.",
+            "Imigranci są końcowo pozytywni.",
+            "Sprawiają mi przyjemność niektóre zagraniczne kultury."
+        ],
+        [//-2
+            "Nie ma kultur lepszych od innych.",
+            "Powinniśmy być bardziej akceptujący dla innych kultur.",
+            "Wszystkie społeczeństwa są mieszanką kultur."
+        ],
+        [//-3
+            "Wszyscy powinni mówić w przynajmniej dwóch językach.",
+            "Wszyscy ludzie dzielą wspólną tożsamość.",
+            "Nasza kultura byłaby lepsza więcej zmieszanymi innymi kulturami."
+        ],
+        [//-4
+            "Rządy powinny być tak samo przejęte obywatelami zza granicy jak tymi w swoich granicach.",
+            "Każdy, kto chce dostać się do kraju, powinien móc to zrobić.",
+            "Korzyści z otwartych granic przeważyłyby nad kosztami."
+        ]
+    ]];
